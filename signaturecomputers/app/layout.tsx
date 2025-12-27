36d96210dab0a5a1d46d88ff48e68735d40c6b7a
signaturecomputers <signaturecomputers08@gmail.com>
2025-12-27 12:36:18 +0530

    updated image location

--- signaturecomputers/app/layout.tsx
+++ signaturecomputers/app/layout.tsx
@@ -48,7 +48,7 @@ export const metadata: Metadata = {
     siteName: 'Signature Computers',
     images: [
       {
-        url: '/og-image.jpg',
+        url: '/og-image.png',
         width: 1200,
         height: 630,
         alt: 'Signature Computers - Premium Tech Store',
